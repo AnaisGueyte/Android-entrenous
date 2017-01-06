146b53ae38d3d56c3c484120fbd66e91519fd31c
Anais Gueyte <annais.gueyte@gmail.com>
2017-01-06 15:25:35 +0100

    Work on mapview: middle point and markers for places
    Work on mapview: middle point and markers for places, fix radar

--- main/java/com/example/a34011_82_10/entrenous/Form.java
+++ main/java/com/example/a34011_82_10/entrenous/Form.java
@@ -28,6 +28,7 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -39,20 +40,19 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
     private PlaceAutocompleteFragment autocompleteFragment2;
     private TextView attributionsTextView;
     private TextView attributionsTextView2;
-    private String addr1;
-    private String addr2;
+    protected String addr1;
+    protected String addr2;
 
     private String location;
     private String location2;
     private LatLng latlong;
     private LatLng latlong2;
     private LatLng middlepoint;
-    String sMiddlePoint;
 
     private TextView erreurbox;
     private CheckBox coffee;
     private CheckBox bar;
-    private String barcoffee;
+    protected String barcoffee;
 
     private Intent intent;
 
@@ -75,8 +75,9 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
         autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_fragment);
         autocompleteFragment.setOnPlaceSelectedListener(this);
-        autocompleteFragment.setHint("_ _ _ _ _ _");
+        autocompleteFragment.setHint("_ _ _ _ _ _ _ _ _ _ _ _");
         /** END OF THE FIRST ENTRY **/
+
 
 
         /** FOR THE SECOND ENTRY **/
@@ -101,7 +102,7 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                 Log.i("info", "addr2 : " + addr2);
 
                 if (!TextUtils.isEmpty(place.getAttributions())) {
-                    attributionsTextView.setText(Html.fromHtml(place.getAttributions().toString()));
+                    attributionsTextView2.setText(Html.fromHtml(place.getAttributions().toString()));
                 }
             }
 
@@ -109,10 +110,12 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
             public void onError(Status status) {
             }
         });
-        autocompleteFragment2.setHint("_ _ _ _ _ _");
+        autocompleteFragment2.setHint("_ _ _ _ _ _ _ _ _ _ _ _");
 
         /** END OF  HE SECOND ENTRY **/
 
+
+Log.i("info", "avant le bouton launch map");
 
         erreurbox = (TextView) findViewById(R.id.erreurbox);
         Button launchMap = (Button) findViewById(R.id.launchSearch);
@@ -125,8 +128,11 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
             @Override
             public void onClick(View v) {
 
+                Log.i("info", "dansle bouton launch map avant calcul de middlepoint");
+
                 try {
                     midPoint(addr1, addr2);
+                    Log.i("info", "Midpoint apres press button : " + addr1 + addr2);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -145,7 +151,7 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
 
 
                 if ((coffee.isChecked() == true) && (bar.isChecked() == false)) {
-                    barcoffee = " 'cafe' ";
+                    barcoffee = "cafe";
                     erreurbox.setText("");
                     Log.i("cafe check", barcoffee);
                     /* sending the intent */
@@ -160,6 +166,8 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                     Log.i("info", "bundle address2 : " + latlong2);
                     mBundle.putParcelable("middlepoint", middlepoint);
                     Log.i("info", "bundle middlePoint : " + middlepoint);
+                    mBundle.putString("type", barcoffee);
+                    Log.i("info", "bundle bar coffee : " + barcoffee);
 
                     intent.putExtras(mBundle);
 
@@ -167,7 +175,7 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                     startActivity(intent);
                 }
                 if ((bar.isChecked() == true) && (coffee.isChecked() == false)) {
-                    barcoffee = " 'bar' ";
+                    barcoffee = "bar";
                     erreurbox.setText("");
                     Log.i("bar check", barcoffee);
                     /* sending the intent */
@@ -182,6 +190,8 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                     Log.i("info", "bundle address2 : " + latlong2);
                     mBundle.putParcelable("middlepoint", middlepoint);
                     Log.i("info", "bundle middlePoint : " + middlepoint);
+                    mBundle.putString("type", barcoffee);
+                    Log.i("info", "bundle bar coffee : " + barcoffee);
 
                     intent.putExtras(mBundle);
 
@@ -191,7 +201,7 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
 
                 if ((coffee.isChecked() == true) && (bar.isChecked() == true)) {
 
-                    barcoffee = " 'cafe' | 'bar' ";
+                    barcoffee = " cafe | bar ";
                     erreurbox.setText("");
                     Log.i("cafe & bar check", barcoffee);
 
@@ -207,6 +217,8 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                     Log.i("info", "bundle address2 : " + latlong2);
                     mBundle.putParcelable("middlepoint", middlepoint);
                     Log.i("info", "bundle middlePoint : " + middlepoint);
+                    mBundle.putString("type", barcoffee);
+                    Log.i("info", "bundle bar coffee : " + barcoffee);
 
                     intent.putExtras(mBundle);
 
@@ -220,6 +232,8 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
 
     }
 
+
+
     @Override
     public void onPlaceSelected(Place place) {
         Log.i(LOG_TAG, "Place Selected: " + place.getName());
@@ -228,13 +242,18 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
                 .getName(), place.getAddress(), place.getPhoneNumber(), place
                 .getWebsiteUri(), place.getRating(), place.getId()));
 
+        Log.i("info", "apres le attribution textvie de addr1");
+
         addr1 = place.getAddress().toString();
+        Log.i("info", "addr1: " + addr1);
 
         if (!TextUtils.isEmpty(place.getAttributions())) {
             attributionsTextView.setText(Html.fromHtml(place.getAttributions().toString()));
         }
 
     }
+
+
 
 
     @Override
@@ -247,32 +266,43 @@ public class Form extends AppCompatActivity implements PlaceSelectionListener {
 
     public void midPoint(String addr1, String addr2) throws IOException {
 
+        Log.i("info", "dans middle point: ");
+
         Geocoder geocoder = new Geocoder(Form.this);
 
         /*** first address ***/
+        Log.i("info", "dans middle point addr1: " + addr1);
         location = addr1;
-        List<Address> list = geocoder.getFromLocationName(location, 1);
+        List<Address> list = (geocoder.getFromLocationName(location, 1));
         Address add = list.get(0);
         String locality = add.getLocality();
         latlong = new LatLng(add.getLatitude(), add.getLongitude());
 
         /*** second address ***/
+        Log.i("info", "dans middle point addr2: " + addr2);
         location2 = addr2;
         List<Address> list2 = geocoder.getFromLocationName(location2, 1);
         Address add2 = list2.get(0);
         String locality2 = add2.getLocality();
         latlong2 = new LatLng(add2.getLatitude(), add2.getLongitude());
 
+
+        Log.i("info", "dans middle point method resultat de latlong2: " + latlong2);
+
         double lat1 = latlong.latitude;
         double lon1 = latlong.longitude;
         double lat2 = latlong2.latitude;
         double lon2 = latlong2.longitude;
 
+        Log.i("info", "dans middle point method APRES LES DONNEES LAT LONG ");
 
         double latiCentre = (lat1 + lat2) / 2;
         double longCentre = (lon1 + lon2) / 2;
 
+        Log.i("info", "dans middle point method APRES LE CALCUL DE LA MOITIE ");
+
         middlepoint = new LatLng(latiCentre, longCentre);
+        Log.i("info", "dans middle point method resultat de middle point: " + middlepoint);
 
     }
 
--- main/java/com/example/a34011_82_10/entrenous/MapsActivity.java
+++ main/java/com/example/a34011_82_10/entrenous/MapsActivity.java
@@ -1,18 +1,14 @@
 package com.example.a34011_82_10.entrenous;
 
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.support.v4.app.FragmentActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -20,9 +16,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+
+
 
 /**
  * This class will show the map and coffee places requested
@@ -35,14 +42,15 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    protected GoogleMap mMap;
     private Marker marker;
     private Intent i;
     private Bundle b;
 
     private LatLng latlong;
     private LatLng latlong2;
-    private LatLng middlePoint;
+    protected LatLng middlePoint;
+    protected String barcoffee;
 
 
     @Override
@@ -60,6 +68,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         latlong = b.getParcelable("address1");
         latlong2 = b.getParcelable("address2");
         middlePoint = b.getParcelable("middlepoint");
+        barcoffee = b.getString("type");
+        Log.i("info", "bundle dans MAP ACTIVITY bar coffee : " + barcoffee);
+
+
+
+        StringBuilder sbValue = new StringBuilder(sbMethod(barcoffee));
+        PlacesTask placesTask = new PlacesTask();
+        placesTask.execute(sbValue.toString());
+
 
 
         /*** CODE TO INITIALIZE THE MAP ***/
@@ -98,16 +115,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
       MarkerOptions markerOptions = new MarkerOptions()
               .title("Point 1")
+              .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person_outline_black_24dp))
               .position(latlong);
 
       MarkerOptions markerOptions2 = new MarkerOptions()
               .title("Point 2")
+              .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person_outline_black_24dp))
               .position(latlong2);
 
       MarkerOptions middleOptions = new MarkerOptions()
               .title("Mi-chemin")
-              .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_location))
               .position(middlePoint);
+
+
 
       marker = mMap.addMarker(markerOptions);
       marker = mMap.addMarker(markerOptions2);
@@ -116,5 +136,167 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
       Log.i("info", "APRES ADD MARKER");
 
   }
+
+
+
+    /********************************************************************/
+    /******************** PRIVATE CLASSES  ******************************/
+    /********************************************************************/
+
+    public StringBuilder sbMethod(String barcoffee) {
+
+        //use your current location here
+        double mLatitude = middlePoint.latitude;
+        double mLongitude = middlePoint.longitude;
+
+        Log.i("info", " bar coffee dans sbMethod: " + barcoffee);
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        sb.append("location=" + mLatitude + "," + mLongitude);
+        sb.append("&radius=5000");
+        sb.append("&types=" + barcoffee);
+        sb.append("&sensor=true");
+        sb.append("&key=«YOUR KEY»);
+
+        Log.d("Map", "api: " + sb.toString());
+
+        return sb;
+    }
+
+    private class PlacesTask extends AsyncTask<String, Integer, String> {
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result) {
+            ParserTask parserTask = new ParserTask();
+
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParserTask
+            parserTask.execute(result);
+        }
+
+        private String downloadUrl(String strUrl) throws IOException {
+            String data = "";
+            InputStream iStream = null;
+            HttpURLConnection urlConnection = null;
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.19.254.1", 8080));
+
+            try {
+                URL url = new URL(strUrl);
+
+                // Creating an http connection to communicate with url
+                urlConnection = (HttpURLConnection) url.openConnection(proxy);
+
+                // Connecting to url
+                urlConnection.connect();
+
+                // Reading data from url
+                iStream = urlConnection.getInputStream();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+                StringBuffer sb = new StringBuffer();
+
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                data = sb.toString();
+
+                br.close();
+
+            } catch (Exception e) {
+                Log.d("Exception download url", e.toString());
+            } finally {
+                iStream.close();
+                urlConnection.disconnect();
+            }
+            return data;
+        }
+
+        private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
+
+            JSONObject jObject;
+
+            // Invoked by execute() method of this object
+            @Override
+            protected List<HashMap<String, String>> doInBackground(String... jsonData) {
+
+                List<HashMap<String, String>> places = null;
+                Place_JSON placeJson = new Place_JSON();
+
+                try {
+                    jObject = new JSONObject(jsonData[0]);
+
+                    places = placeJson.parse(jObject);
+
+                } catch (Exception e) {
+                    Log.d("Exception", e.toString());
+                }
+                return places;
+            }
+
+            // Executed after the complete execution of doInBackground() method
+            @Override
+            protected void onPostExecute(List<HashMap<String, String>> list) {
+
+                Log.d("Map", "list size: " + list.size());
+                // Clears all the existing markers;
+               /* mMap.clear();*/
+
+                for (int i = 0; i < list.size(); i++) {
+
+                    // Creating a marker
+                    MarkerOptions markerOptions = new MarkerOptions();
+
+                    // Getting a place from the places list
+                    HashMap<String, String> hmPlace = list.get(i);
+
+
+                    // Getting latitude of the place
+                    double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                    // Getting longitude of the place
+                    double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                    // Getting name
+                    String name = hmPlace.get("place_name");
+
+                    Log.d("Map", "place: " + name);
+
+                    // Getting vicinity
+                    String vicinity = hmPlace.get("vicinity");
+
+                    LatLng latLng = new LatLng(lat, lng);
+
+                    // Setting the position for the marker
+                    markerOptions.position(latLng);
+
+                    markerOptions.title(name + " : " + vicinity);
+
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+                    // Placing a marker on the touched position
+                    Marker m = mMap.addMarker(markerOptions);
+
+                }
+            }
+        }
+
+    }
 
 }
